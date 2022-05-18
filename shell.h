6cd32cc9ae3acadc8b58c767a75c218dd17fae5f
Je4stine <je4stine@gmail.com>
2022-05-18 13:48:21 +0100

    Here we go

--- shell.h
+++ shell.h
@@ -1,28 +1,33 @@
-#ifndef shell_h
-#define shell_h
-#include <stddef.h>
-#include <sys/stat.h>
-#include <signal.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
+#ifndef _SHELL_H_
+#define _SHELL_H_
 #include <stdio.h>
-#include <sys/wait.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
 #include <fcntl.h>
-#include <stddef.h>
+#include <limits.h>
+#include <time.h>
+
 extern char **environ;
-#define DELIMIT " \t\r\n\a"
-#define SIZE 1024
-#define PATH_MAX 1020
-int _getline();
-int _strlen(char *s);
-char _strdup(char *str);
-int _strcmp(char *s1, char *s2);
-int _strtok();
-void _fork();
-void sig_handler(int signum);
-volatile sig_atomic_t flag = 0;
-int _stat(int ac, char **av);
+
+int _strcmp(char *str1, char *str2);
+int get_path(char **arguments);
+int getbuiltins(char **arguments, int exit_stat);
+char *_getenv(char *global_var);
+char *_strdup(char *str);
+char *append_command(char *dir_path, char *command);
+int _mprint(const char *prompt, unsigned int size);
+void free_mem(char *entry);
+int getcmd_inputs(char *entry, char **arguments);
+int err_stat(int n);
+int p_error(char **arguments, int counter);
 int _putchar(char c);
+int check_file(char *pathname);
+int execute(char **arguments);
+void free_grid(char **grid, int heigth);
+int _strlen(const char *str);
+
 #endif
